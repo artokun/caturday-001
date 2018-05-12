5e7fd29a3feb58d010fe18e7715d5b6fd30373b3
artokun <art.longbottom.jr@gmail.com>
2018-05-12 15:17:43 -0700

    example test

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const MONGO_URI =
-  process.env.MONGO_URI || 'mongodb://localhost:27017/caturday-test';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI);
